d090dbbd4acf6e7031b741309bd177594521faa6
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 16:40:03 +0100

    Debug messages.

--- scripts/linux-requisites-install.sh
+++ scripts/linux-requisites-install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -x -e
+
+echo Check os release
 . /etc/os-release
 
+echo Get sudo ($(command -v sudo))
 SUDO=$(command -v sudo)
 
 case $ID in
     debian|ubuntu)
+        echo update
         $SUDO apt-get -qq update
         # $SUDO apt-get -qqy dist-upgrade
+        echo install
         $SUDO apt-get -qqy install \
             libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev \
             libgsl-dev build-essential cmake git libjpeg-dev \
