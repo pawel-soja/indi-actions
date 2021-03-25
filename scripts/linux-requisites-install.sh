1fb38469976cfb4de8fd56d0c017458b1ffce5c1
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 16:54:15 +0100

    WIP

--- scripts/linux-requisites-install.sh
+++ scripts/linux-requisites-install.sh
@@ -5,19 +5,11 @@ set -x -e
 echo Check os release
 . /etc/os-release
 
-VAR=2
-echo Get sudo $(command -v sudo)
-#SUDO2=$(command -v sudo)
-
-echo "Case $ID"
-
 case $ID in
     debian|ubuntu)
-        echo update
-        apt-get update
-        # $SUDO apt-get -qqy dist-upgrade
-        echo install
-        apt-get -y install \
+        $(command -v sudo) apt-get update
+        # $(command -v sudo) apt-get -qqy dist-upgrade
+        $(command -v sudo) apt-get -y install \
             libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev \
             libgsl-dev build-essential cmake git libjpeg-dev \
             libcurl4-gnutls-dev libtiff-dev libfftw3-dev
@@ -28,5 +20,3 @@ case $ID in
         exit 1
         ;;
 esac
-
-exit 0
