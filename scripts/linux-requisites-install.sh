4382ea2657a91b768634029f9f66b60556464542
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 16:55:54 +0100

    Dont use SUDO variable, set -e.

--- scripts/linux-requisites-install.sh
+++ scripts/linux-requisites-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-. /etc/os-release
+set -e
 
-SUDO=$(command -v sudo)
+. /etc/os-release
 
 case $ID in
     debian|ubuntu)
-        $SUDO apt-get -qq update
-        $SUDO apt-get -qqy dist-upgrade
-        $SUDO apt-get -qqy install \
+        $(command -v sudo) apt-get -qq update
+        $(command -v sudo) apt-get -qqy dist-upgrade
+        $(command -v sudo) apt-get -qqy install \
             libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev \
             libgsl-dev build-essential cmake git libjpeg-dev \
             libcurl4-gnutls-dev libtiff-dev libfftw3-dev
